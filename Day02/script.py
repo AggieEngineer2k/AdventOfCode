43bb97abb541095ddb3cc2b9b665772c0c890e72
Justin L. Brown <brownjl@cpchem.com>
2022-12-14 08:09:39 -0600

    Finished Day 2 challenge

--- Day02/script.py
+++ Day02/script.py
@@ -1,42 +1,87 @@
+opponent_rock = 'A'
+opponent_paper = 'B'
+opponent_scissor = 'C'
+
+you_rock = 'X'
+you_paper = 'Y'
+you_scissor = 'Z'
+
+you_lose = 'X'
+you_draw = 'Y'
+you_win = 'Z'
+
 def getOutcome(opponent,you):
     loss = 0
     draw = 3
     win = 6
-    if opponent == 'A': # Rock
-        if you == 'X': # Rock
+    if opponent == opponent_rock:
+        if you == you_rock:
             return draw
-        elif you == 'Y': # Paper
+        elif you == you_paper:
             return win
-        elif you == 'Z': # Scissor
+        elif you == you_scissor:
             return loss
-    elif opponent == 'B': # Paper
-        if you == 'X': # Rock
+    elif opponent == opponent_paper:
+        if you == you_rock:
             return loss
-        elif you == 'Y': # Paper
+        elif you == you_paper:
             return draw
-        elif you == 'Z': # Scissor
+        elif you == you_scissor:
             return win
-    elif opponent == 'C': # Scissor
-        if you == 'X': # Rock
+    elif opponent == opponent_scissor:
+        if you == you_rock:
             return win
-        elif you == 'Y': # Paper
+        elif you == you_paper:
             return loss
-        elif you == 'Z': # Scissor
+        elif you == you_scissor:
             return draw
 
+def lose(opponent):
+    if opponent == opponent_rock:
+        return you_scissor
+    elif opponent == opponent_paper:
+        return you_rock
+    elif opponent == opponent_scissor:
+        return you_paper
+
+def draw(opponent):
+    if opponent == opponent_rock:
+        return you_rock
+    elif opponent == opponent_paper:
+        return you_paper
+    elif opponent == opponent_scissor:
+        return you_scissor
+
+def win(opponent):
+    if opponent == opponent_rock:
+        return you_paper
+    elif opponent == opponent_paper:
+        return you_scissor
+    elif opponent == opponent_scissor:
+        return you_rock
+
 def score(opponent,you):
-    if you == 'X': # Rock
-        return getOutcome(opponent,you) + 1
-    elif you == 'Y': # Paper
-        return getOutcome(opponent,you) + 2
-    elif you == 'Z': # Scissor
-        return getOutcome(opponent,you) + 3
+    outcome = getOutcome(opponent,you)
+    if you == you_rock:
+        return outcome + 1
+    elif you == you_paper:
+        return outcome + 2
+    elif you == you_scissor:
+        return outcome + 3
     
 total = 0
 with open('input.txt') as f:
     lines = f.readlines()
     for line in lines:
         play = line.strip().split()
-        total += score(play[0],play[1])
+        # Part 1
+        #total += score(play[0],play[1])
+        # Part 2
+        if play[1] == you_lose:
+            total += score(play[0],lose(play[0]))
+        elif play[1] == you_draw:
+            total += score(play[0],draw(play[0]))
+        elif play[1] == you_win:
+            total += score(play[0],win(play[0]))
 
 print(total)
