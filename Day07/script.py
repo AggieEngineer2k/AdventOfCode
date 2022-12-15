dc20293b6de4e3282969b75993230bcb48f3bfb1
Justin L. Brown <brownjl@cpchem.com>
2022-12-15 15:21:19 -0600

    Day 07 Part 1

--- Day07/script.py
+++ Day07/script.py
@@ -1,13 +1,17 @@
-from anytree import search, Node, RenderTree, find
+from anytree import Node, RenderTree, PreOrderIter
 
-root = Node("root")
+root = Node('root', type='dir')
 
 def processLine(line : str, node : Node) -> Node:
     if line[0] == '$':
         return processCommand(line, node)
     elif line.startswith('dir'):
         name = line.split()[1]
-        Node(name, parent=node)
+        Node(name, parent=node, type='dir')
+        return node
+    elif line[0].isdigit():
+        tokens = line.split()
+        Node(tokens[1], parent=node, type='file', size=int(tokens[0]))
         return node
     else:
         return node
@@ -23,13 +27,24 @@ def processCommand(line : str, node : Node) -> Node:
     else:
         return node
 
+def getDirectorySize(node : Node) -> int:
+    size = 0
+    for file in [node for node in PreOrderIter(node, filter_=lambda n: hasattr(n, 'type') and n.type == 'file')]:
+        size += file.size
+    return size
+
 lines = list()
 with open('input.txt','r') as f:
     lines = [line.rstrip() for line in f]
 
-node = root
+currentNode = root
 for line in lines:
-    print(line)
-    node = processLine(line, node)
+    currentNode = processLine(line, currentNode)
 
-print(RenderTree(root))
+totalSize = 0
+for node in [node for node in PreOrderIter(root, filter_=lambda n: hasattr(n, 'type') and n.type == 'dir')]:
+    size = getDirectorySize(node)
+    if size <= 100000:
+        totalSize += size
+
+print(totalSize)
