5d27c504d39b81748201c07362a1cd134268819e
Justin L. Brown <AggieEngineer2k@hotmail.com>
2022-12-15 19:46:22 -0600

    Day 08 Parts 1 and 2

--- Day08/script.py
+++ Day08/script.py
@@ -1,26 +1,54 @@
-def visible_north(row,column,height):
+def isVisibleFromNorth(row,column,height):
     if row == 0:
         return forest[row][column] < height
     else:
-        return forest[row][column] < height and visible_north(row - 1,column,height)
+        return forest[row][column] < height and isVisibleFromNorth(row - 1,column,height)
 
-def visible_south(row,column,height):
+def isVisibleFromSouth(row,column,height):
     if row == rows - 1:
         return forest[row][column] < height
     else:
-        return forest[row][column] < height and visible_south(row + 1,column,height)
+        return forest[row][column] < height and isVisibleFromSouth(row + 1,column,height)
 
-def visible_east(row,column,height):
+def isVisibleFromEast(row,column,height):
     if column == columns - 1:
         return forest[row][column] < height
     else:
-        return forest[row][column] < height and visible_east(row,column + 1,height)
+        return forest[row][column] < height and isVisibleFromEast(row,column + 1,height)
 
-def visible_west(row,column,height):
+def isVisibleFromWest(row,column,height):
     if column == 0:
         return forest[row][column] < height
     else:
-        return forest[row][column] < height and visible_west(row,column - 1,height)
+        return forest[row][column] < height and isVisibleFromWest(row,column - 1,height)
+
+def scoreFromNorth(row,column,height,score : int = 0) -> int:
+    score += 1
+    if row == 0 or forest[row][column] >= height:
+        return score
+    else:
+        return scoreFromNorth(row - 1,column,height,score)
+
+def scoreFromSouth(row,column,height,score : int = 0) -> int:
+    score += 1
+    if row == rows - 1 or forest[row][column] >= height:
+        return score
+    else:
+        return scoreFromSouth(row + 1,column,height,score)
+
+def scoreFromEast(row,column,height,score : int = 0) -> int:
+    score += 1
+    if column == columns - 1 or forest[row][column] >= height:
+        return score
+    else:
+        return scoreFromEast(row,column + 1,height,score)
+
+def scoreFromWest(row,column,height,score : int = 0) -> int:
+    score += 1
+    if column == 0 or forest[row][column] >= height:
+        return score
+    else:
+        return scoreFromWest(row,column - 1,height,score)
 
 lines = None
 with open('input.txt','r') as input:
@@ -32,12 +60,31 @@ for line in lines:
 
 rows = len(forest)
 columns = len(forest[0])
+
 visible_trees = 0
-for row in range(0,rows):
-    for column in range(0,columns):
+for row in range(rows):
+    for column in range(columns):
         if row == 0 or row == rows - 1 or column == 0 or column == columns - 1:
             visible_trees += 1
-        elif visible_north(row - 1,column,forest[row][column]) or visible_south(row + 1,column,forest[row][column]) or visible_east(row,column + 1,forest[row][column]) or visible_west(row,column - 1,forest[row][column]):
+        elif isVisibleFromNorth(row - 1,column,forest[row][column]) \
+            or isVisibleFromSouth(row + 1,column,forest[row][column]) \
+            or isVisibleFromEast(row,column + 1,forest[row][column]) \
+            or isVisibleFromWest(row,column - 1,forest[row][column]):
             visible_trees += 1
 
-print(visible_trees)
+print(f"Part 1: {visible_trees}")
+
+scores = [[None] * columns for i in range(rows)]
+for row in range(rows):
+    for column in range(columns):
+        if row == 0 or row == rows - 1 or column == 0 or column == columns - 1:
+            scores[row][column] = 0
+        else:
+            height = forest[row][column]
+            scores[row][column] = \
+                scoreFromNorth(row - 1,column,height) \
+                * scoreFromSouth(row + 1,column,height) \
+                * scoreFromEast(row,column + 1,height) \
+                * scoreFromWest(row,column - 1,height)
+
+print(f"Part 2: {max(map(max, scores))}")
