c562667b9f7613d871dbaa0e953742ad17e0d231
Justin L. Brown <AggieEngineer2k@hotmail.com>
2022-12-19 23:21:56 -0600

    Able to create graph of vertices.

--- 2022/Day12/script.py
+++ 2022/Day12/script.py
@@ -1,11 +1,44 @@
-class Solver:
-    def __init__(self, map) -> None:
-        self.map = map
+class Vertex:
+    def __init__(self, height : str) -> None:
+        self.height = height
+        self.visited = False
+        self.distance = float('inf')
+        # Reference to the previous vertex in the shortest path.
+        self.parent : Vertex = None
+        # Reference to the starting vertex.
+
+class Graph:
+    def __init__(self, vertices) -> None:
+        # Store the graph of vertices.
+        self.vertices = [[Vertex(column) for column in row] for row in vertices]
+        # Create a set for storing the vertices in the shortest path.
+        self.shortest_path_tree = set()
+
+    def findVerticesByHeight(self,height : str):
+        return [(vertex,irow,icolumn) for irow, row in enumerate(self.vertices) for icolumn, vertex in enumerate(row) if vertex.height == height]
+
+    def findShortestPath(self,starting : str,ending : str):
+        # Find the starting vertex.
+        start = self.findVerticesByHeight(starting)[0][0]
+        # Set the start vertex's distance to 0.
+        start.distance = 0
+
+        # Find the ending vertex.
+        end = self.findVerticesByHeight(ending)[0][0]
+
+        # Start searching from the starting vertex and continue searching until the ending vertex is the next to be visited.
+        current = start
+        #while current != end:
+
     def show(self):
-        [print(''.join([str(c) for c in r])) for r in self.map]
+        [print(' '.join([str(vertex.height) for vertex in row])) for row in self.vertices]
+        [print(' '.join([str(vertex.visited) for vertex in row])) for row in self.vertices]
+        [print(' '.join([str(vertex.distance) for vertex in row])) for row in self.vertices]
 
-solver : Solver
+graph : Graph
 with open('input.txt','r') as inputFile:
-    solver = Solver([list(line.rstrip()) for line in inputFile])
+    graph = Graph(vertices=[list(line.rstrip()) for line in inputFile])
 
-solver.show()
+graph.findShortestPath(starting='S',ending='E')
+
+graph.show()
