b5a7ff76040d6a6ddd0526d642dc89c4a4d959fd
Justin L. Brown <AggieEngineer2k@hotmail.com>
2022-12-21 09:45:02 -0600

    refactored part 1 for sortable results

--- 2022/Day13/script.py
+++ 2022/Day13/script.py
@@ -3,40 +3,41 @@ import logging
 logging.basicConfig(level=logging.WARN)
 indent_step = 2
 
-def testPairIsInRightOrder(left, right, indent = 0) -> bool:
+def testPairIsInRightOrder(left, right, indent = 0) -> int:
     logging.info(f"{' ' * indent}- Compare {left} vs {right}")
     left_is_an_int = isinstance(left,int)
     right_is_an_int = isinstance(right,int)
     if left_is_an_int and right_is_an_int:
         indent += indent_step
         if left < right:
-            logging.debug(f'True: {left} <= {right}')
+            logging.debug(f'True: {left} < {right}')
             logging.info(f"{' ' * indent}- Left side is smaller, so inputs are in the right order")
-            return True
+            return -1
         elif left > right:
             logging.debug(f'False: {left} > {right}')
             logging.info(f"{' ' * indent}- Right side is smaller, so inputs are not in the right order")
-            return False
+            return 1
         else:
-            return None
+            return 0
     elif not left_is_an_int and not right_is_an_int:
         length = min(len(left),len(right))
         i = 0
-        test = None
-        while i < length and test == None:
+        test = 0
+        while i < length and test == 0:
             test = testPairIsInRightOrder(left[i],right[i],indent + indent_step)
+            logging.debug(f'test: {test}')
             i += 1
-        if test != None:
+        if test != 0:
             return test
         else:
             if len(left) < len(right):
                 logging.info(f"{' ' * indent}- Left side ran out of items, so inputs are in the right order")
-                return True
+                return -1
             elif len(left) > len(right):
                 logging.info(f"{' ' * indent}- Right side ran out of items, so inputs are not in the right order")
-                return False
+                return 1
             else:
-                return None
+                return 0
     elif left_is_an_int and not right_is_an_int:
         logging.info(f"{' ' * indent}- Mixed types; convert left to {[left]} and retry comparison")
         return testPairIsInRightOrder([left],right,indent + indent_step)
@@ -53,6 +54,8 @@ for i in range(len(lines) // 2):
     left = ast.literal_eval(lines[i * 2])
     right = ast.literal_eval(lines[i * 2 + 1])
     is_pair_in_right_order = testPairIsInRightOrder(left,right)
-    if is_pair_in_right_order:
+    logging.debug(is_pair_in_right_order)
+    if is_pair_in_right_order == -1:
         pairs_in_right_order.append(i + 1)
 print(f'Part 1: {pairs_in_right_order} = {sum(pairs_in_right_order)}')
+
