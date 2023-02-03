0f4cd1918a0be463b8bb13ff48f5015a5a9c54cf
Justin L. Brown <brownjl@cpchem.com>
2023-02-03 14:05:19 -0600

    2015 Day 4 done

--- 2015/day04/script.py
+++ 2015/day04/script.py
@@ -8,13 +8,18 @@ class script:
         self.input = input
     def get_hash_hexdigest(self, key : str, number : int) -> str:
         return hashlib.md5(f"{key}{number}".encode()).hexdigest()
+    def find_number_for_hash_startswith(self, key : str, startswith : str):
+        i = 1
+        while i < 9999999:
+            if self.get_hash_hexdigest(key, i).startswith(startswith):
+                return i
+            i += 1
     def day_1(self):
-        for i in range(100000,999999):
-            if self.get_hash_hexdigest(self.input, i).startswith("000000"):
-                break
-        print(f"Day 1: {i}")
+        number = self.find_number_for_hash_startswith(self.input, "0" * 5)
+        print(f"Day 1: {number}")
     def day_2(self):
-        print(f"Day 2: ")
+        number = self.find_number_for_hash_startswith(self.input, "0" * 6)
+        print(f"Day 2: {number}")
 
 input_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'input.txt'))
 with open(input_path,'r') as inputFile:
--- 2015/day04/tests/test_2015_day04_script.py
+++ 2015/day04/tests/test_2015_day04_script.py
@@ -2,6 +2,11 @@ import pytest
 from day04.script import script
 
 @pytest.mark.parametrize("key,number,startswith", [("abcdef",609043,"000001dbbfa"),("pqrstuv",1048970,"000006136ef")])
-def test_Santa_move(key, number, startswith):
+def test_get_hash_hexdigest(key, number, startswith):
     actual = script().get_hash_hexdigest(key,number)
     assert actual.startswith(startswith)
+
+@pytest.mark.parametrize("key,startswith,expected", [("abcdef","0" * 5,609043),("pqrstuv","0" * 5,1048970)])
+def test_find_number_for_hash_startswith(key, startswith, expected):
+    actual = script().find_number_for_hash_startswith(key,startswith)
+    assert actual == expected
