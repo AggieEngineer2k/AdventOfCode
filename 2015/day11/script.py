1583a18a4b69ecb0f2de6efa23ee221b2631391c
Justin L. Brown <brownjl@cpchem.com>
2023-05-18 14:54:13 -0500

    implemetation and tests

--- 2015/day11/script.py
+++ 2015/day11/script.py
@@ -6,8 +6,29 @@ import logging
 logging.basicConfig(level=logging.WARN)
 
 class Script:
+    def check_contains_straight(self, input) -> bool:
+        for substring in [input[i:i+3] for i in range(0,len(input)-2)]:
+            if (ord(substring[0]) == ord(substring[1]) - 1 and ord(substring[1]) == ord(substring[2]) - 1):
+                return True
+        return False
+    def sanitize_string(self, input) -> str:
+        index = -1
+        for x in ['i','o','l']:
+            i = input.find(x)
+            if (i > -1 and (index == -1 or i < index)):
+                index = i
+        result = input
+        if (index > -1):
+            result = result[:index] + chr(ord(result[index]) + 1) + (((len(result) - index - 1)) * 'a')
+        return result
     def increment_string(self, input) -> str:
-        return input
+        if (input == 'z'):
+            result = 'aa'
+        elif (input[-1] == 'z'):
+            result = self.increment_string(input[:-1]) + 'a'
+        else:
+            result = input[:-1] + chr(ord(input[-1]) + 1)
+        return result
     def __init__(self, input : str = ""):
         self.input = input
     def day_1(self):
--- 2015/day11/tests/test_2015_day11_script.py
+++ 2015/day11/tests/test_2015_day11_script.py
@@ -1,6 +1,10 @@
 import pytest
 from day11.script import Script
 
+# Passwords must include one increasing straight of at least three letters, like abc, bcd, cde, and so on, up to xyz. They cannot skip letters; abd doesn't count.
+# Passwords may not contain the letters i, o, or l, as these letters can be mistaken for other characters and are therefore confusing.
+# Passwords must contain at least two different, non-overlapping pairs of letters, like aa, bb, or zz.
+
 # hijklmmn meets the first requirement (because it contains the straight hij) but fails the second requirement requirement (because it contains i and l).
 # abbceffg meets the third requirement (because it repeats bb and ff) but fails the first requirement.
 # abbcegjk fails the third requirement, because it only has one double letter (bb).
@@ -10,13 +14,42 @@ from day11.script import Script
 # Incrementing is just like counting with numbers: xx, xy, xz, ya, yb, and so on.
 # Increase the rightmost letter one step;
 #     if it was z, it wraps around to a, and repeat with the next letter to the left until one doesn't wrap around.
-@pytest.mark.parametrize("input,expected", [
-    ("a","b"),
-    ("z","aa"),
-    ("xx","xy"),
-    ("xy","xz"),
-    ("xz","ya"),
-    ("ya","yb"),
+
+@pytest.mark.parametrize('input,expected', [
+    ('abc',True),
+    ('bcd',True),
+    ('cde',True),
+    ('xyz',True),
+    ('xabcx',True),
+    ('xbcdx',True),
+    ('xcdex',True),
+    ('xxyzx',True),
+    ('aabbcc',False)
+])
+def test_check_contains_straight(input : str, expected : bool):
+    script = Script()
+    actual = script.check_contains_straight(input)
+    assert actual == expected
+
+@pytest.mark.parametrize('input,expected', [
+    ('xix','xja'),
+    ('xxoxx','xxpaa'),
+    ('xxxlxxx','xxxmaaa'),
+    ('ghijklmn','ghjaaaaa')
+])
+def test_script_sanitize_string(input : str, expected : str):
+    script = Script()
+    actual = script.sanitize_string(input)
+    assert actual == expected
+
+@pytest.mark.parametrize('input,expected', [
+    ('a','b'),
+    ('z','aa'),
+    ('xx','xy'),
+    ('xy','xz'),
+    ('xz','ya'),
+    ('ya','yb'),
+    ('zz','aaa'),
 ])
 def test_script_increment_string(input : str, expected : str):
     script = Script()
