a16e9eb6197bd0aa4e9014736a6936599d3dcc5a
Justin L. Brown <AggieEngineer2k@hotmail.com>
2023-05-20 06:02:45 -0500

    2015 day 12

--- 2015/day12/script.py
+++ 2015/day12/script.py
@@ -4,18 +4,37 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardi
 from common.input_parser import InputParser
 import logging
 logging.basicConfig(level=logging.WARNING)
-import re
+import json
 
 class Script:
-    def extract_numbers(self, input) -> "list[int]":
-        return [int(x) for x in re.findall('(-?\d+)',input)]
+    def iterate(self, element, total : int = 0, skipRed : bool = False) -> int:
+        if isinstance(element, list):
+            for i in element:
+                total = self.iterate(i, total, skipRed)
+            return total
+        elif isinstance(element, dict):
+            if (skipRed == False or 'red' not in list(element.values())):
+                for (k,v) in element.items():
+                    total = self.iterate(v, total, skipRed)
+            return total
+        elif isinstance(element, str):
+            return total
+        elif isinstance(element, int):
+            return total + element
+        else:
+            raise Exception(f'Unsupported element type "{type(element)}"')
+    def parse_input(self, input):
+        return json.loads(input)
     def __init__(self, input : str = ""):
         self.input = input
     def day_1(self):
-        result = sum(self.extract_numbers(self.input))
-        print(f"Day 1: {result}")
+        jsonObject = self.parse_input(self.input)
+        total = self.iterate(jsonObject)
+        print(f"Day 1: {total}")
     def day_2(self):
-        print(f"Day 2: ")
+        jsonObject = self.parse_input(self.input)
+        total = self.iterate(jsonObject, skipRed=True)
+        print(f"Day 2: {total}")
 
 input_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'input.txt'))
 input = InputParser.parse_line(input_path)
--- 2015/day12/tests/test_2015_day12_script.py
+++ 2015/day12/tests/test_2015_day12_script.py
@@ -3,15 +3,49 @@ from day12.script import Script
 
 @pytest.mark.parametrize('input,expected', [
     ('[1,2,3]',[1,2,3]),
-    ('{"a":2,"b":4}',[2,4]),
-    ('[[[3]]]',[3]),
-    ('{"a":{"b":4},"c":-1}',[4,-1]),
-    ('{"a":[-1,1]}',[-1,1]),
-    ('[-1,{"a":1}]',[-1,1]),
-    ('[]',[]),
-    ('{}',[]),
+    ('{"a":1, "b":2}',{"a":1,"b":2}),
 ])
-def test_extract_numbers(input : str, expected : "list[int]"):
+def test_parse_input(input : str, expected):
     script = Script()
-    actual = script.extract_numbers(input)
+    actual = script.parse_input(input)
+    assert actual == expected
+    
+# Part 1
+#
+# [1,2,3] and {"a":2,"b":4} both have a sum of 6.
+# [[[3]]] and {"a":{"b":4},"c":-1} both have a sum of 3.
+# {"a":[-1,1]} and [-1,{"a":1}] both have a sum of 0.
+# [] and {} both have a sum of 0.
+
+@pytest.mark.parametrize('input,expected', [
+    ([1,2,3], 6),
+    ({"a":2,"b":4}, 6),
+    ([[[3]]],3),
+    ({"a":{"b":4},"c":-1},3),
+    ({"a":[-1,1]},0),
+    ([-1,{"a":1}],0),
+    ([],0),
+    ({},0),
+])
+def test_iterate(input, expected : int):
+    script = Script()
+    actual = script.iterate(input)
+    assert actual == expected
+
+# Part 2
+#
+# [1,2,3] still has a sum of 6.
+# [1,{"c":"red","b":2},3] now has a sum of 4, because the middle object is ignored.
+# {"d":"red","e":[1,2,3,4],"f":5} now has a sum of 0, because the entire structure is ignored.
+# [1,"red",5] has a sum of 6, because "red" in an array has no effect.
+
+@pytest.mark.parametrize('input,expected', [
+    ([1,2,3], 6),
+    ([1,{"c":"red","b":2},3], 4),
+    ({"d":"red","e":[1,2,3,4],"f":5},0),
+    ([1,"red",5],6),
+])
+def test_iterate_skipRed(input, expected : int):
+    script = Script()
+    actual = script.iterate(input, skipRed=True)
     assert actual == expected
