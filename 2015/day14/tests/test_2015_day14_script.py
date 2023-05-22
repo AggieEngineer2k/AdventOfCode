f363427344373e5b7193b8d07c545cef84e951dc
Justin L. Brown <brownjl@cpchem.com>
2023-05-22 15:17:45 -0500

    2015 Day 14 init

---
+++ 2015/day14/tests/test_2015_day14_script.py
@@ -0,0 +1,10 @@
+import pytest
+from day13.script import Script
+
+# @pytest.mark.parametrize('input,expected', [
+#     ('Alice would gain 54 happiness units by sitting next to Bob.',('Alice','Bob',54)),
+# ])
+# def test_parse_edge(input : str, expected : tuple):
+#     script = Script()
+#     actual = script.parse_edge(input)
+#     assert actual == expected
