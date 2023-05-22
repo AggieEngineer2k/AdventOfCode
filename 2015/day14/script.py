13b645065d8feb79d61f667c5b8707a977951f30
Justin L. Brown <brownjl@cpchem.com>
2023-05-22 16:29:21 -0500

    2015 Day 14 Part 1

--- 2015/day14/script.py
+++ 2015/day14/script.py
@@ -4,12 +4,50 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardi
 from common.input_parser import InputParser
 import logging
 logging.basicConfig(level=logging.WARNING)
+import re
+from typing import NamedTuple
+
+class Reindeer:
+    name : str
+    speed_km_s : int
+    fly_s : int
+    rest_s : int
+    def __init__(self, name : str = "", speed_km_s : int = 0, fly_s : int = 0, rest_s : int = 0, input : str = ""):
+        self.name = name
+        self.speed_km_s = speed_km_s
+        self.fly_s = fly_s
+        self.rest_s = rest_s
+        regex = r"(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds."
+        if re.match(regex, input):
+            result = re.search(regex, input)
+            self.name = result.group(1)
+            self.speed_km_s = int(result.group(2))
+            self.fly_s = int(result.group(3))
+            self.rest_s = int(result.group(4))
+    def distance_traveled(self, time : int) -> int:
+        cycle = self.fly_s + self.rest_s
+        completions = time // cycle
+        partials = time % cycle
+        distance = ((self.fly_s * completions) + min(self.fly_s, partials)) * self.speed_km_s
+        return distance
+    def __eq__(self, other): 
+        if not isinstance(other, Reindeer):
+            return NotImplemented
+        return self.name == other.name \
+            and self.speed_km_s == other.speed_km_s \
+            and self.fly_s == other.fly_s \
+            and self.rest_s == other.rest_s
 
 class Script:
     def __init__(self, input : "list[str]" = []):
         self.input = input
-    def day_1(self):
-        print(f"Day 1: ")
+    def day_1(self, time : int):
+        winner = 0
+        for line in self.input:
+            reindeer = Reindeer(input = line)
+            distance = reindeer.distance_traveled(time)
+            winner = max(winner, distance)
+        print(f"Day 1: {winner}")
     def day_2(self):
         print(f"Day 2: ")
 
@@ -17,5 +55,5 @@ input_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'input.txt
 input = InputParser.parse_lines(input_path)
 
 script = Script(input)
-script.day_1()
+script.day_1(2503)
 script.day_2()
--- 2015/day14/tests/test_2015_day14_script.py
+++ 2015/day14/tests/test_2015_day14_script.py
@@ -1,10 +1,18 @@
 import pytest
-from day13.script import Script
+from day14.script import Script,Reindeer
 
-# @pytest.mark.parametrize('input,expected', [
-#     ('Alice would gain 54 happiness units by sitting next to Bob.',('Alice','Bob',54)),
-# ])
-# def test_parse_edge(input : str, expected : tuple):
-#     script = Script()
-#     actual = script.parse_edge(input)
-#     assert actual == expected
+@pytest.mark.parametrize('input,expected', [
+    ('Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.',Reindeer('Comet',14,10,127)),
+    ('Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.',Reindeer('Dancer',16,11,162)),
+])
+def test_Reindeer_init(input : str, expected : Reindeer):
+    actual = Reindeer(input = input)
+    assert actual == expected
+
+@pytest.mark.parametrize('reindeer,time,expected', [
+    (Reindeer('Comet',14,10,127),1000,1120),
+    (Reindeer('Dancer',16,11,162),1000,1056),
+])
+def test_Reindeer_distance_traveled(reindeer : Reindeer, time : int, expected : int):
+    actual = reindeer.distance_traveled(time)
+    assert actual == expected
