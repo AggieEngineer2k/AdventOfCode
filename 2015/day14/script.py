92003df2a1f04bed3deeab402ead72b3502e71f0
Justin L. Brown <brownjl@cpchem.com>
2023-05-23 09:32:18 -0500

    2015 Day 14

--- 2015/day14/script.py
+++ 2015/day14/script.py
@@ -12,11 +12,15 @@ class Reindeer:
     speed_km_s : int
     fly_s : int
     rest_s : int
+    points : int
+    distance : int
     def __init__(self, name : str = "", speed_km_s : int = 0, fly_s : int = 0, rest_s : int = 0, input : str = ""):
         self.name = name
         self.speed_km_s = speed_km_s
         self.fly_s = fly_s
         self.rest_s = rest_s
+        self.points = 0
+        self.distance = 0
         regex = r"(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds."
         if re.match(regex, input):
             result = re.search(regex, input)
@@ -37,23 +41,58 @@ class Reindeer:
             and self.speed_km_s == other.speed_km_s \
             and self.fly_s == other.fly_s \
             and self.rest_s == other.rest_s
+    def __str__(self) -> str:
+        return f"{self.name} has {self.points} points after traveling {self.distance} km."
+    
+class Race:
+    def __init__(self, input : "list[str]" = []):
+        self.racers = dict()
+        for line in input:
+            reindeer = Reindeer(input = line)
+            self.racers[reindeer.name] = reindeer
+    def run_race(self, time_s : int):
+        racer : Reindeer
+        # Initialize race to starting line.
+        for name in self.racers:
+            self.racers[name].points = 0
+            self.racers[name].distance = 0
+        # Run the race one second at a time.
+        for i in range(1,time_s + 1):
+            logging.debug(f"=== {i} s ===")
+            for name in self.racers:
+                racer = self.racers[name]
+                racer.distance = racer.distance_traveled(i)
+                logging.debug(f"{racer.name} has traveled {racer.distance} km.")
+            max_distance = max([self.racers[name].distance for name in self.racers])
+            logging.debug(f"Leaders are at {max_distance} km.")
+            for name in self.racers:
+                racer = self.racers[name]
+                if(racer.distance == max_distance):
+                   racer.points += 1
+                   logging.debug(f"{racer.name} now has {racer.points} points.")
+    def get_point_winners(self) -> "list[Reindeer]":
+        max_points = max([self.racers[name].points for name in self.racers])
+        return [self.racers[name] for name in self.racers if self.racers[name].points == max_points]
 
 class Script:
     def __init__(self, input : "list[str]" = []):
         self.input = input
     def day_1(self, time : int):
-        winner = 0
+        distances = []
         for line in self.input:
             reindeer = Reindeer(input = line)
-            distance = reindeer.distance_traveled(time)
-            winner = max(winner, distance)
-        print(f"Day 1: {winner}")
-    def day_2(self):
-        print(f"Day 2: ")
+            distances.append(reindeer.distance_traveled(time))
+        max_distance = max(distances)
+        print(f"Day 1: {max_distance}")
+    def day_2(self, time : int):
+        race = Race(self.input)
+        race.run_race(time)
+        winners = race.get_point_winners()
+        print(f"Day 2: {winners[0].points}")
 
 input_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ),'input.txt'))
 input = InputParser.parse_lines(input_path)
 
 script = Script(input)
 script.day_1(2503)
-script.day_2()
+script.day_2(2503)
--- 2015/day14/tests/test_2015_day14_script.py
+++ 2015/day14/tests/test_2015_day14_script.py
@@ -1,5 +1,5 @@
 import pytest
-from day14.script import Script,Reindeer
+from day14.script import Script,Reindeer,Race
 
 @pytest.mark.parametrize('input,expected', [
     ('Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.',Reindeer('Comet',14,10,127)),
@@ -10,9 +10,34 @@ def test_Reindeer_init(input : str, expected : Reindeer):
     assert actual == expected
 
 @pytest.mark.parametrize('reindeer,time,expected', [
+    (Reindeer('Comet',14,10,127),1,14),
+    (Reindeer('Dancer',16,11,162),1,16),
+    (Reindeer('Comet',14,10,127),10,140),
+    (Reindeer('Dancer',16,11,162),10,160),
+    (Reindeer('Comet',14,10,127),11,140),
+    (Reindeer('Dancer',16,11,162),11,176),
+    (Reindeer('Comet',14,10,127),12,140),
+    (Reindeer('Dancer',16,11,162),12,176),
     (Reindeer('Comet',14,10,127),1000,1120),
     (Reindeer('Dancer',16,11,162),1000,1056),
 ])
 def test_Reindeer_distance_traveled(reindeer : Reindeer, time : int, expected : int):
     actual = reindeer.distance_traveled(time)
     assert actual == expected
+
+@pytest.fixture
+def race():
+    return Race([
+        'Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.',
+        'Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.',
+    ])
+
+@pytest.mark.parametrize('time,expected', [
+    (1,{"Dancer":1,"Comet":0}),
+    (140,{"Dancer":139,"Comet":1}),
+    (1000,{"Dancer":689,"Comet":312}),
+])
+def test_Race_run_race(race, time : int, expected):
+    race.run_race(time)
+    for name in expected:
+        assert race.racers[name].points == expected[name]
